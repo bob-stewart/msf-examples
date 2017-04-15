0ff8deee1d33c35f524e18855c06ecbbe1cf0dbf
Archis Gore <archis@concurix.com>
2017-04-15 16:56:23 -0700

    Added a detailed redirection condition

--- ssl/appdef.js
+++ ssl/appdef.js
@@ -60,6 +60,28 @@ app = function() {
         }
     };
 
+    var redirectorWithPortRouteInfo = {
+        RouteType:          4,
+        ID:                 "redirector",
+        Timeout:            365 * 24 * 60 * 60 * 1000000000,
+        ContainerChain: {
+            ConnectionDrainGracePeriod: 5 * 1000000000,
+            PerInstanceTimeout:         30 * 1000000000,
+            DesiredInstances:           2,
+            IsStateless:                true,
+            Chain: [
+                {
+                    BaseImage:          "polyverse-internal.jfrog.io/redirect:latest",
+                    HealthCheckURLPath: "/health",
+                    LaunchGracePeriod:  10 * 1000000000,
+                    Cmd:                ["/entrypoint.sh","-bind=80","-scheme=https","-port=8443"],
+                    Env:                ["PVSCRAMBLE=true"], //looks like empty param not handled; errors in log
+                    BindingPort:        80,
+                }
+            ]
+        }
+    };
+
     return {
         Name: function(c) {
             return "SslDemo";
@@ -68,14 +90,20 @@ app = function() {
             return true;
         },
         Route: function(r, c) {
+            var host = r.Host;
             var proto = r.Header['X-Forwarded-Proto']; //Set by upstream LB
 
 
             // ok iff https or AWS ELB health check, else redirect
             if ( r.TLS || 'https' == proto ) {
-                return defaultRouteInfo
+                return defaultRouteInfo;
             } else {
-                return redirectorRouteInfo
+                //if host has a port in it, then reset port
+                if (host.indexOf(":") >= 0) {
+                  return redirectorWithPortRouteInfo
+                } else {
+                  return redirectorRouteInfo
+                }
             }
         }
     };
