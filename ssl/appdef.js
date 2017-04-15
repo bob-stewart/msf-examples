bb31f46206f01ac40ae4d786b1431470ebfcaa78
Archis Gore <archis@concurix.com>
2017-04-15 12:04:24 -0700

    Added appdef and images

---
+++ ssl/appdef.js
@@ -0,0 +1,87 @@
+app = function() {
+    /*
+     * NOTE! pv* variables will be subject to change by downstream processes.
+     * These are dev friendly defaults.
+     */
+    var pvModsecImage   = 'polyverse-tools.jfrog.io/modsecurity:latest';
+    var pvRedirectImage = 'polyverse-internal.jfrog.io/redirect:0d31234d945e6e9f7c15f28493a747fc1d6d8030';
+
+    var defaultRouteInfo = {
+        RouteType:          4,
+        ID:                 "secured_microservice",
+        Timeout:            365 * 24 * 60 * 60 * 1000000000,
+        ContainerChain: {
+            ConnectionDrainGracePeriod: 5 * 1000000000,
+            PerInstanceTimeout:         30 * 1000000000,
+            DesiredInstances:           3,
+            IsStateless:                true,
+            Chain: [
+                {
+                    BaseImage:          "polyverse-tools.jfrog.io/modsecurity:latest",
+                    HealthCheckURLPath: "/health" ,
+                    LaunchGracePeriod:  60 * 1000000000,
+                    Cmd:                [
+                                            "/entrypoint.sh",
+                                            "-proxy-pass=http://next:8080", //'next' will be passed to next link in the chain
+                                            "-SecRuleEngine=On",
+                                        ],
+                    Env:                [],
+                    BindingPort:        8080
+                },
+                {
+                    BaseImage:          "tutum/hello-world:latest",
+                    HealthCheckURLPath: "/",
+                    LaunchGracePeriod:  10 * 1000000000,
+                    BindingPort:        8080,
+                }
+            ]
+        }
+    };
+
+    var redirectorRouteInfo = {
+        RouteType:          4,
+        ID:                 "redirector",
+        Timeout:            365 * 24 * 60 * 60 * 1000000000,
+        ContainerChain: {
+            ConnectionDrainGracePeriod: 5 * 1000000000,
+            PerInstanceTimeout:         30 * 1000000000,
+            DesiredInstances:           2,
+            IsStateless:                true,
+            Chain: [
+                {
+                    BaseImage:          "polyverse-internal.jfrog.io/redirector:latest",
+                    HealthCheckURLPath: "/health",
+                    LaunchGracePeriod:  10 * 1000000000,
+                    Cmd:                ["/entrypoint.sh","-bind=80","-scheme=https"],
+                    Env:                ["PVSCRAMBLE=true"], //looks like empty param not handled; errors in log
+                    BindingPort:        80,
+                }
+            ]
+        }
+    };
+
+    return {
+        Name: function(c) {
+            return "website";
+        },
+        IsRequestSupported: function(r, c) {
+            return true;
+        },
+        Route: function(r, c) {
+            var host  = r.Host;
+            var path  = r.URL.Path;
+            var agent = r.Header['User-Agent'];
+            var proto = r.Header['X-Forwarded-Proto']; //Set by upstream LB
+
+            c.Log.Infof("%v", r);
+
+            // ok iff https or AWS ELB health check, else redirect
+            if ( 'https' == proto )
+            {
+                return defaultRouteInfo
+            } else {
+                return redirectorRouteInfo
+            }
+        }
+    };
+}();
--- ssl/test.sh
+++ ssl/test.sh
@@ -1,9 +1,20 @@
-echo "Reading latest known published tag."
-tag=$(cat ../tag)
-echo "Using tag: $tag"
+echo "Reading latest known published MSF."
+msf=$(cat ../msf)
+echo "Using MSF: $msf"
 
 echo "Pulling Microservice Firewall (be sure you are logged into to docker registry.)"
-docker pull store/polyverse/microservicefirewall:$tag
+docker pull $msf
 
-echo "Running the bare-simple Microservice firewall mode"
-docker run --rm -it --name polyverse_microservice_firewall -p 8080:8080 --privileged store/polyverse/microservicefirewall:$tag
+echo "Running Microservice firewall to expose the image $image behind SSL."
+echo "This demonstrates the power of a downloading from images.json, a custom configuration, and a custom Application Definition that routes intelligently"
+
+docker run --rm -it \
+      --name polyverse_microservice_firewall \
+      -p 8080:8080 \
+      --privileged \
+      -v $PWD/ssk.key:/secrets/ssl.key \
+      -v $PWD/ssl.crt:/secrets/ssl.crt \
+      -v $PWD/polyverse.yaml:/polyverse.yaml \
+      -v $PWD/appdef.js:/appdef.ks \
+      -v $PWD/images.json:/images.json \
+      $msf
