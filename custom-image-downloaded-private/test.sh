9cb6ccd3a1d54b638dff06a47b58fbb176ec061d
Archis Gore <archis@concurix.com>
2017-04-15 10:12:25 -0700

    Added credential mounting example

---
+++ custom-image-downloaded-private/test.sh
@@ -0,0 +1,19 @@
+echo "Reading latest known published tag."
+tag=$(cat ../tag)
+echo "Using tag: $tag"
+
+echo "Pulling Microservice Firewall (be sure you are logged into to docker registry.)"
+docker pull store/polyverse/microservicefirewall:$tag
+
+image=$1
+if [[ "$image" == "" ]]; then
+	echo "This script takes in a private image to which you already have pull access on this docker instance."
+	echo "Since you haven't provided one, defaulting to a public hello world image."
+	image="tutum/hello-world"
+fi
+
+echo "Running the Microservice firewall over privately available image: $image"
+echo "This is done by mounting ~/.docker/config.json to /root/.docker/config.json in the MSF container,"
+echo "allowing the inner daemon to pull that image."
+
+docker run --rm -it --name polyverse_microservice_firewall -p 8080:8080 --privileged -v ~/.docker/config.json:/root/.docker/config.json store/polyverse/microservicefirewall:$tag "$image"
