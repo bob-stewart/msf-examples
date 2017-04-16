d3087ac91552f6c7221ca88203b841bf4d035246
Archis Gore <archis@concurix.com>
2017-04-15 17:50:21 -0700

    Custom images from tarball example
    Removing a tarball

--- custom-image-from-tarball/test.sh
+++ custom-image-from-tarball/test.sh
@@ -7,7 +7,16 @@ echo "Using MSF: $msf"
 echo "Pulling Microservice Firewall (be sure you are logged into to docker registry.)"
 docker pull $msf
 
-echo "Running Microservice firewall with a custom app definition file."
+echo "Downloading an example image: tutum/hello-world"
+docker pull tutum/hello-world
+
+echo "Saving it to a tar file"
+docker save tutum/hello-world -o images.tar
+
+echo "Gzipping the tarball"
+gzip images.tar
+
+echo "Running Microservice firewall with private images loaded from a tarball."
 
 docker run --rm -it \
       --name polyverse_microservice_firewall \
@@ -15,5 +24,5 @@ docker run --rm -it \
       -p 8443:8443 \
       --privileged \
       -v $PWD/appdef.js:/appdef.js \
-      -v $PWD/images.json:/images.json \
+      -v $PWD/images.tar.gz:/images.tar.gz \
       $msf
