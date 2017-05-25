66917ec2df0507506de2bef8adc0c4ef0ee0c3ba
rsundahl <roy@polyverse.io>
2017-05-24 18:56:43 -0700

    Simplified to use polyverse/standalone.

--- wrapped-mc/appdef.js
+++ wrapped-mc/appdef.js
@@ -1,47 +1,31 @@
 app = function() {
 
   var customerChainComponentProducer = 
-    function(imageName, bindingPort, wrapinfo) {
+    function(imageName, bindingPort, wrapInfo) {
       return {
-        BaseImage:          imageName, 
-        Env:                wrapinfo.envs,
-        ExtraHosts:         wrapinfo.hosts,
+        BaseImage:          imageName,
+        Args:               wrapInfo.args,
+        Env:                wrapInfo.envs,
+        ExtraHosts:         wrapInfo.hosts,
+        Mounts:             wrapInfo.mounts,
         HealthCheckURLPath: "/",
         LaunchGracePeriod:  5 * 60 * 1000000000,
         BindingPort:        parseInt(bindingPort)
       };
     };
 
-  var modSecComponentProducer = 
-    function(bindingPort) {
-      return {
-        BaseImage:          "polyverse-tools.jfrog.io/modsecurity:7d99f4dfe9683e27f207a81d1de73be255dd967f",
-        HealthCheckURLPath: "/health" ,
-        LaunchGracePeriod:  60 * 1000000000,
-        Cmd: [
-          "/entrypoint.sh",
-          "-proxy-pass=http://next:" + bindingPort,
-          "-SecRuleEngine=On"
-        ],
-        Env:                [],
-        BindingPort:        8080
-      };
-    };
-
   var routeInfoProducer =
-    function(imageName, bindingPort, wrapinfo) {
+    function(imageName, bindingPort, wrapInfo) {
       return {
         RouteType:                  4,
-        ID:                         "default_route",
-        Timeout:                    365 * 24 * 60 * 60 * 1000000000,
+        ID:                         "default-route",
+        Timeout:                    30 * 1000000000,
         ContainerChain: {
-          ConnectionDrainGracePeriod: 3 * 60 * 1000000000,
-          PerInstanceTimeout:         300 * 1000000000,
+          ConnectionDrainGracePeriod: 60 * 60 * 1000000000,
+          PerInstanceTimeout:         60 * 1000000000,
           DesiredInstances:           2,
-          IsStateless:                true,
           Chain: [
-            //modSecComponentProducer(bindingPort),
-            customerChainComponentProducer(imageName, bindingPort, wrapinfo)
+            customerChainComponentProducer(imageName, bindingPort, wrapInfo)
           ]
         }
       };
@@ -49,23 +33,23 @@ app = function() {
 
   return {
     Name: function() {
-      return "default_appdef";
+      return "default-appdef";
     },
     IsRequestSupported: function(r,c) {
       return true;
     },
     Route: function(r,c) {
-      var bindingport = c.Kv.Get("binding_port");
-      var imagename   = c.Kv.Get("image_name");
-      var wrapinfo    = c.Kv.Get("wrapinfo");
-      var wrapinfoj   = JSON.parse(wrapinfo);
+      var bindingPort = c.Kv.Get("bindingPort");
+      var imageName   = c.Kv.Get("imageName");
+      var wrapInfo    = c.Kv.Get("wrapInfo");
+      var wrapInfoJ   = JSON.parse(wrapInfo);
 
-      c.Log.Infof("Image Name: %s", imagename);
-      c.Log.Infof("Binding Port: %d", bindingport);
-      c.Log.Infof("wrapinfo: %s", wrapinfo);
-      c.Log.Infof("wrapinfoj: %s", wrapinfoj);
+      c.Log.Infof("imageName: %s", imageName);
+      c.Log.Infof("bindingPort: %d", bindingPort);
+      c.Log.Infof("wrapInfo: %s", wrapInfo);
+      c.Log.Info(wrapInfoJ);
 
-      return routeInfoProducer(imagename, bindingport, wrapinfoj);
+      return routeInfoProducer(imageName, bindingPort, wrapInfoJ);
     },
     ValidationInfo: function() {
       return {
@@ -75,3 +59,4 @@ app = function() {
     }
   };
 }();
+
--- wrapped-mc/test.sh
+++ wrapped-mc/test.sh
@@ -1,7 +1,11 @@
 #! /bin/sh
 
+# Pull the extra images that we need
+docker pull polyverse/standalone:latest
+docker pull polyverse/emcee:latest
+
 # First run emcee as a straight-through http/https server
-docker run --rm --name=wrapped-mc --privileged -d -p 80:8080 wrapped-mc -redirect=false
+docker run -it --rm --name=wrapped-mc --privileged -e SA_BINDING_PORT=80 -e SA_NO_PULL=TRUE -v /var/run/docker.sock:/var/run/outer_docker.sock -v $PWD/appdef.js:/appdef.js -d -p 80:8080 polyverse/standalone:latest polyverse/emcee:latest -redirect=false
 
 echo "Testing redirection disabled"
 
@@ -17,6 +21,9 @@ curl http://localhost
 
 echo "Testing websocket upgrade over HTTP: "
 curl --header "Connection: Upgrade" --header "Upgrade: websocket" --header "Host: localhost" --header "Origin: http://localhost/websocket" --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" --header "Sec-WebSocket-Version: 13" http://localhost/websocket
+
+echo "Testing websocketecho over HTTP: "
+curl --header "Connection: Upgrade" --header "Upgrade: websocket" --header "Host: localhost" --header "Origin: http://localhost/websocket" --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" --header "Sec-WebSocket-Version: 13" http://localhost/websocketecho
 
 docker kill wrapped-mc > /dev/null 2>&1
 
