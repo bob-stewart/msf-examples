df1028c2ba4d84be5fca365989193b15bae1ccf6
rsundahl <roy@polyverse.io>
2017-05-25 20:36:24 -0700

    Create a separate route for each remote session.

--- wrapped-mc/appdef.js
+++ wrapped-mc/appdef.js
@@ -15,11 +15,11 @@ app = function() {
     };
 
   var routeInfoProducer =
-    function(imageName, bindingPort, wrapInfo) {
+    function(routeId, imageName, bindingPort, wrapInfo) {
       return {
         RouteType: 4,
-        ID:        "default-route",
-        Timeout:   30 * 1000000000,
+        ID:        routeId,
+        Timeout:   10 * 1000000000,
         ContainerChain: {
           Stateful:  true,
           ConnectionDrainGracePeriod: 60 * 60 * 1000000000,
@@ -32,23 +32,27 @@ app = function() {
 
   return {
     Name: function() {
-      return "default-appdef";
+      return "wrapped-mc";
     },
     IsRequestSupported: function(r,c) {
       return true;
     },
     Route: function(r,c) {
+      var routeId     = r.RemoteAddr.replace(/[^a-zA-Z0-9]/g,'-').replace(/-{2,}/g, '-');
       var bindingPort = c.Kv.Get("bindingPort");
       var imageName   = c.Kv.Get("imageName");
       var wrapInfo    = c.Kv.Get("wrapInfo");
       var wrapInfoJ   = JSON.parse(wrapInfo);
 
+
+      c.Log.Infof("r: %s", r);
+      c.Log.Infof("routeId: %s", routeId);
       c.Log.Infof("imageName: %s", imageName);
       c.Log.Infof("bindingPort: %d", bindingPort);
       c.Log.Infof("wrapInfo: %s", wrapInfo);
       c.Log.Info(wrapInfoJ);
 
-      return routeInfoProducer(imageName, bindingPort, wrapInfoJ);
+      return routeInfoProducer(routeId, imageName, bindingPort, wrapInfoJ);
     },
     ValidationInfo: function() {
       return {
